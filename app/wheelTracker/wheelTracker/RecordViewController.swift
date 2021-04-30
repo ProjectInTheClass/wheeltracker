844ec5f4447edc1dc67868defc770a0df827f779
jbhus <gksrudtn99@gmail.com>
2021-04-30 17:47:20 +0900

    create storyboards

---
+++ app/wheelTracker/wheelTracker/RecordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecordViewController.swift
+//  wheeltracker
+//
+//  Created by 한경수 on 2021/04/30.
+//
+
+import UIKit
+import Charts
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
