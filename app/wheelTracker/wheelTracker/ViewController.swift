e5da4c468f13ef64abc0e4b98abde06d9ba0e2f2
jbhus <gksrudtn99@gmail.com>
2021-05-07 17:11:14 +0900

    Main.storyboard

--- app/wheelTracker/wheelTracker/ViewController.swift
+++ app/wheelTracker/wheelTracker/ViewController.swift
@@ -14,9 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var distance: UILabel!
     @IBOutlet weak var calories: UILabel!
     @IBOutlet weak var duration: UILabel!
+    @IBOutlet weak var monthPushCount: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadUserData()
+    }
+    
+    func loadUserData (){
         let todayData = pushDatas.filter{
             let nowCalendar = Calendar.current.dateComponents([.year, .month, .day], from: Date())
             let dataCalendar = Calendar.current.dateComponents([.year, .month, .day], from: $0.createdAt)
@@ -30,10 +35,22 @@ class ViewController: UIViewController {
         dayPushCount.text = String(todayData[0].pushCount)
         distance.text = String(Int(todayData[0].distance)) + "m"
         calories.text = String(todayData[0].calorie) + "kcal"
-        let format = DateFormatter()
-        format.dateFormat = "HH:mm"
-        duration.text = String(Int(todayData[0].duration/60)) + ":" + String(Int(todayData[0].duration)%60)
         
+        duration.text = String(Int(todayData[0].duration/60)) + "m" + String(Int(todayData[0].duration)%60) + "s"
+        
+        let monthDataCounts = pushDatas.filter{
+            let nowCalendar = Calendar.current.dateComponents([.year, .month, .day], from: Date())
+            let dataCalendar = Calendar.current.dateComponents([.year, .month, .day], from: $0.createdAt)
+            if nowCalendar.year == dataCalendar.year && nowCalendar.month == dataCalendar.month {
+                return true
+            }
+            else{
+                return false
+            }
+        }.reduce(0){
+            $0+$1.pushCount
+        }
+        monthPushCount.text = String(monthDataCounts)
     }
 
 
