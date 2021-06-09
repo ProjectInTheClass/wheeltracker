ce2274e64fe0368b8a292a8dc67ec2e2ed46abea
여충관 <76516825+te06008@users.noreply.github.com>
2021-06-10 06:33:04 +0900

    setting 페이지 영점조절 완료 (#33)

--- app/wheelTracker/wheelTracker/ZeroingViewController.swift
+++ app/wheelTracker/wheelTracker/ZeroingViewController.swift
@@ -13,7 +13,7 @@ class ZeroingViewController: UIViewController {
     @IBOutlet weak var start: UIButton!
     
     var startDistance = pushDatas[pushDatas.count-1].distance
-    var nowDistance = pushDatas[pushDatas.count-1].distance {didSet{manageDistance()}}
+    var nowDistance = pushDatas[pushDatas.count-1].distance
     
     
     override func viewDidLoad() {
@@ -39,12 +39,38 @@ class ZeroingViewController: UIViewController {
     @IBAction func start(_ sender: Any) {
         startDistance = pushDatas[pushDatas.count-1].distance
         nowDistance = pushDatas[pushDatas.count-1].distance
-        
+        distanceEvent.listenTo(eventName: "distance", action: self.manageDistance)
     }
     
     
     func manageDistance(){
-        zeroing.text = String(Int(nowDistance-startDistance))
+        nowDistance = pushDatas[pushDatas.count-1].distance
+        zeroing.text = String(Int(nowDistance-startDistance)) + "m"
+        zeroing.font = zeroing.font.withSize(50)
+        if(nowDistance - startDistance >= 20){
+            let alert = UIAlertController(title: "측정 완료", message: "몇번 밀었나요?", preferredStyle: .alert)
+
+            let ok = UIAlertAction(title: "입력", style: .default) { (ok) in
+                let countOfPush = alert.textFields?[0].text
+                let cop = countOfPush!
+                let cop2 = Int(cop)!
+                unitDistance = Double(20)/Double(cop2)
+                print(unitDistance)
+            }
+
+            let cancel = UIAlertAction(title: "재측정", style: .cancel) { (cancel) in
+    
+            }
+
+            alert.addAction(ok)
+            alert.addAction(cancel)
+            alert.addTextField{(mytextField) in
+                mytextField.placeholder = "숫자만 입력해주세요."
+            }
+            
+            self.present(alert, animated: true, completion: nil)
+            distanceEvent.removeListeners(eventNameToRemoveOrNil: "distance")
+        }
     }
     
 }
